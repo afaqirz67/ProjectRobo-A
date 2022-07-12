641bbb2b92e040d5167123305c8c0b5def7b9454
Josh Robson Chase <josh@robsonchase.com>
2022-07-12 17:58:50 -0400

    Add test program

---
+++ robot/test.c
@@ -0,0 +1,38 @@
+#include "anim.h"
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+void computeLegAnimations(struct Leg* leg) {
+  computeTransitions(&leg->animation);
+}
+
+void setup() {
+	doLegs(computeLegAnimations);
+}
+
+void loop() {
+
+}
+
+int now = 0;
+int last = 0;
+
+int main(void) {
+	setup();
+
+	for(int i = 0; i < 1000; i++) {
+		for(int l = 0; l < NUM_LEGS; l++) {
+			legs[l]->counter += 1000;
+			struct Animation anim = legs[l]->animation;
+			struct AnimationFrame frame = computeState(anim, &legs[l]->counter);
+			for(int s = 0; s < SERVO_LEN; s++) {
+				printf("leg %d, servo %d: %g\n", l, s, frame.servos[s]);
+			}
+		}
+	}
+}
